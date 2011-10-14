f52ad2841c47b8d9dcef14d82eb28e0556c923b0
lilin <yjh521@gmail.com>
2011-10-14 20:45:25 +0800

    now can get fuquan index now, next step is calculate fuquaned price

--- fuquan.py
+++ fuquan.py
@@ -1,26 +1,37 @@
 # coding=utf-8
+from readDayRecord import *
 """
-if  start time < infotime < endtime
-    bonus
-    freecounter_rate
-    cheapercounter_rate
-    cheaperprice
-    laterprice = earlierprice 
-    前复权：复权后价格＝[(earlierprice-bonus)＋cheaperprice*cheapercounter]÷(1＋cheaperaccount_rate+freecounter_rate) 
-            fq_1 prince =  (earlierprice-bonus)
-            fq_2 price  =  (fq_1 price)/(1+free_rate)
-            fq_3 = fq_2
-    return fq_3
-else
+
 """    
 
-STDTFQinfo = [[20110617, 0.3, 0, 0.6, 0, 0]]
+STDTFQinfo = [[20110617, 0.3, 0, 0.6, 0, 0],
+        [20100617, 0.3, 0, 0.6, 0, 0],
+        [20090617, 0.3, 0, 0.6, 0, 0],
+        [20080617, 0.3, 0, 0.6, 0, 0],
+        [20070617, 0.3, 0, 0.6, 0, 0]]
 
-def SingleFuqian_Forward(FQinfo):
+def GetMatchedFuquanFromWholeFuquan(starttime, endtime, WholeFuquanData):
+    recordNumber = len(WholeFuquanData)
+    if len(WholeFuquanData) == 0:
+        return []
+    part1 = []
+    part2 = []
+    for i in WholeFuquanData:
+        print i
+        print "input end time:" + str(endtime)
+        if i[0] < endtime:
+            part1.append(i)
+
+    for i in part1:
+        if i[0] > starttime:
+            part2.append(i)
+    #part2 is Fuquan info between start and endtime
+    return part2
+def SingleFuqian_Forward(FQinfo, filepointer):
     """FQinfo is : [time(trade time), free(how many), gift(How many), bonus(how much), cheaper_price(how much), cheaper_count(how many)]
     """
     print FQinfo
-    earlierprice = 27.54
+    earlierprice = GetYesterDayEndPrice(filepointer, FQinfo[0])
     bonus = FQinfo[3]
     free = FQinfo[1]
     gift = FQinfo[2]
@@ -30,12 +41,18 @@ def SingleFuqian_Forward(FQinfo):
     fq_2 = (fq_1)/(1 + free + gift) 
     fq_3 = ((fq_2) + cheaper_price*cheaper_count)/(1+cheaper_count)
     return fq_3/earlierprice 
-def periodFuquan_factor_Forward(FQPeriodTable):
+def periodFuquan_factor_Forward(FQPeriodTable, filepointer):
     result = 1
     if type(FQPeriodTable[0]) == type([]):
         for i in FQPeriodTable:
-            result = result * SingleFuqian_Forward(i)
+            result = result * SingleFuqian_Forward(i, filepointer)
     else:
-        result = result * SingleFuqian_Forward(FQPeriodTable)
+        result = result * SingleFuqian_Forward(FQPeriodTable, filepointer)
     return result
-print periodFuquan_factor_Forward(STDTFQinfo)
+#print periodFuquan_factor_Forward(STDTFQinfo)
+print GetMatchedFuquanFromWholeFuquan(20050930, 20110430, STDTFQinfo)
+print GetMatchedFuquanFromWholeFuquan(20050930, 20110430, [])
+print GetMatchedFuquanFromWholeFuquan(20050930, 20110930, [[20110617, 0.3, 0, 0.6, 0, 0]])
+KdayFileName = 'sh\\lday\\sh600834.day'
+filepointer = open(KdayFileName, 'rb')
+print periodFuquan_factor_Forward(GetMatchedFuquanFromWholeFuquan(20090930, 20110930, STDTFQinfo), filepointer)
