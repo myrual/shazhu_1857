bdc36acec742c95e2288507afc67f246afa9e59e
lilin <yjh521@gmail.com>
2011-10-20 22:25:09 +0800

    add 1017 as milestone and zz100 zz200 zz500

--- All_compare.bat
+++ All_compare.bat
@@ -1,1 +1,1 @@
-python multiblock.py 20110121 20110415 20110617 20110715 20110808 20111011 399300 601857 601398 601318 bx dl dlsb dq dzxx fdc fzfz gcjz gsgq gt hghx hjg htjg jc jsj jtgj jtss jx jycm lyjd mhg mtsy njsp qs qtxy syls tljj tx wm xny ydyb ysjs yswl yxl MTHY
+python multiblock.py 20110121 20110415 20110617 20110715 20110808 20111011 20111017 399300 601857 601398 601318 399903 399904 399905 bx dl dlsb dq dzxx fdc fzfz gcjz gsgq gt hghx hjg htjg jc jsj jtgj jtss jx jycm lyjd mhg mtsy njsp qs qtxy syls tljj tx wm xny ydyb ysjs yswl yxl MTHY
