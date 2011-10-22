d316201723b22dcd70a33004425ae3570546fb43
lilin <yjh521@gmail.com>
2011-10-22 22:10:40 +0800

    add new file to generate today and yesterday compare

---
+++ All_compare_today.bat
@@ -0,0 +1,1 @@
+python multiblock.py 20111011 20111017 20111020 399300 601857 601398 601318 399903 399904 399905 bx dl dlsb dq dzxx fdc fzfz gcjz gsgq gt hghx hjg htjg jc jsj jtgj jtss jx jycm lyjd mhg mtsy njsp qs qtxy syls tljj tx wm xny ydyb ysjs yswl yxl MTHY
